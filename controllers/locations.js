75a1957c9a672acae854321f5949f167b66ed934
calvansaw <calvansaw@hotmail.com>
2021-06-03 23:46:27 +0800

    protect create, edit and delete location API

--- controllers/locations.js
+++ controllers/locations.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const locations = express.Router();
+const Middleware = require('../middlewares');
 const Location = require('../models/locations');
 
 ///////////////////////////////////////////////////////////////////////////////////
 
 ////// Create //////
-locations.post('/', (req, res) => {
+locations.post('/', Middleware.checkAuth, (req, res) => {
 	Location.create(req.body, (err, createdLocation) => {
 		if (err) {
 			res.status(400).json({ message: 'post not successful ' + err });
@@ -64,7 +65,7 @@ locations.get('/', (req, res) => {
 });
 
 ////// Update //////
-locations.put('/:id', (req, res) => {
+locations.put('/:id', Middleware.checkAuth, (req, res) => {
 	Location.findByIdAndUpdate(
 		req.params.id,
 		req.body,
@@ -82,7 +83,7 @@ locations.put('/:id', (req, res) => {
 });
 
 ////// Delete //////
-locations.delete('/:id', (req, res) => {
+locations.delete('/:id', Middleware.checkAuth, (req, res) => {
 	Location.findByIdAndRemove(req.params.id, (err, deletedLocation) => {
 		if (err) {
 			res.status(400).json({ message: 'delete not successful' });
